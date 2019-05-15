bb3319dbfde961afd0887232826230bfb8036b99
AndreaGarcesEPN <50632356+AndreaGarcesEPN@users.noreply.github.com>
2019-05-15 10:25:16 -0500

    Función main completa

--- SlnCalculatorTutorial/CalculatorTutorial/Calculator.cpp
+++ SlnCalculatorTutorial/CalculatorTutorial/Calculator.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "Calculator.h"
-
+#include<stdio.h>
 
 /*Calculator::Calculator()
 {
@@ -13,5 +13,25 @@ Calculator::~Calculator()
 
 double Calculator::Calculate(double x, char oper, double y)
 {
+	double resultado;
+	switch (oper)
+	{
+	case'+':
+		resultado = x + y;
+		break;
+	case'-':
+		resultado = x - y;
+		break;
+	case'*':
+		resultado = x * y;
+		break;
+	case'/':
+		resultado = x / y;
+		break;
+	default:
+		printf("%s", "Error");
+		break;
+	}
+	return resultado;
 	return 0.0;
 }
--- SlnCalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
+++ SlnCalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -1,12 +1,26 @@
 #include "pch.h"
+#include "Calculator.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Calculator Console Application" << endl << endl;
-	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '\0';
+	cout << "--- Calculadora de consola ---" << endl << endl;
+	cout << "Ingrese la operación a realizar. Formato: a+b | a-b | a*b | a/b"
 		<< endl;
+	cin >> x >> oper >> y;
+
+	Calculator calcular;
+	while (true)
+	{
+		result = calcular.Calculate(x, oper, y);
+		cout << "El resultado es: " << result << endl;
+		break;
+	}
 	return 0;
 }
